a600904964162b98f05c748215979b02d8f0731c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-11-06 10:25:01 -0500

    injecting coordinates into texture and overwriting LUT

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 // Author: @patriciogv 2015
 
-// ============================================= VARIABLES
-//
-var timeDiff = 0;
-var cloudOffset = [0,0];
-var offset_target = [0,0];
-
-// ISS TLL lines
 var ISS = {     
             name: 'ISS',
             tleLine1: '1 25544U 98067A   15305.48861694  .00009749  00000-0  15091-3 0  9998',
@@ -52,28 +45,7 @@ function init() {
     map.setView([0, 0], 3);
     // Scene initialized
     layer.on('init', function() {
-        console.log("Creating orbit and cheching on WebGL ")
-        
-        // If the browser don't suport big textures, reload scene using LowDefenition images
-        if ( scene.gl.getParameter(scene.gl.MAX_TEXTURE_SIZE) < 10800) {
-            console.log("Warning, Browser don't suport big images, reloading style with smaller images");
-            setDefinition("ld");
-        } else {
-            if (window.devicePixelRatio === 1) {
-                setDefinition("hd");
-            } else {
-                initOrbit();
-            }
-        }
-        
-        // window.setInterval("updateSun()", 1000);
-        // window.setInterval("updateClouds()", 100);
-
-        if (window.DeviceMotionEvent) {
-            window.addEventListener("devicemotion", onMotionUpdate, false);
-        }
-        document.addEventListener('mousemove', onMouseUpdate, false);
-        document.addEventListener('mouseenter', onMouseUpdate, false);
+        initOrbit();
     });
     layer.addTo(map);
 }
@@ -82,66 +54,83 @@ function initOrbit() {
     addOrbitToTangramSource("orbits", [ISS, SCUBE]);
 }
 
-// ============================================= UPDATE
+function initTexture() {
+    var sat = ISS;
 
-Date.prototype.getJulian = function() {
-    return Math.floor((this / 86400000) - (this.getTimezoneOffset()/1440) + 2440587.5);
-}
-
-function updateSun() {   // time in seconds since Jan. 01, 1970 UTC
-    // Update Sun position
-    var now = new Date();
-    var cur_hour = now.getHours();
-    var cur_min = now.getMinutes();
-    var cur_sec = now.getSeconds();
-    var cur_jul = now.getJulian() - 1;
-    var equinox_jul = new Date(now.getFullYear(),2,20,24,-now.getTimezoneOffset(),0,0).getJulian() - 1;
-
-    var offset_x = 27-Math.round(((cur_hour*3600 + cur_min*60 + cur_sec)/86400) * 180 ); // Resulting offset X
-    var offset_sin = ((365.25 - equinox_jul + cur_jul)%365.25)/365.25; // Day offset, mapped on the equinox offset
-    var offset_sin_factor = Math.sin(offset_sin * 2 * Math.PI); // Sine wave offset
-    var offset_y = offset_sin_factor * 23.44; // Map onto angle. Maximum angle is 23.44° in both directions
-
-    var sunPos = [offset_x, offset_y]; 
-    scene.styles.earth.shaders.uniforms.u_sun_offset = sunPos;
-    scene.styles.water.shaders.uniforms.u_sun_offset = sunPos;
-}
-
-function updateClouds() {
-    if (scene.styles && 
-        offset_target[0] !== cloudOffset[0] && 
-        offset_target[1] !== cloudOffset[1] ) {
-        cloudOffset[0] += (offset_target[0]-cloudOffset[0])*.25;
-        cloudOffset[1] += (offset_target[1]-cloudOffset[1])*.25;
-        scene.styles.earth.shaders.uniforms.u_clouds_offset = cloudOffset;
-        scene.styles.water.shaders.uniforms.u_clouds_offset = cloudOffset;
+    // Generate the orbit;
+    var track = [];
+    var satrec = satellite.twoline2satrec(sat.tleLine1, sat.tleLine2);
+    var t = new Date();
+    t.setMinutes(t.getMinutes() - 2);
+    var stepSec = 60;
+    for (var i = 0; i < 98; i++) {
+        track.push(getSatellitePositionAt(satrec,t));
+        t.setSeconds(t.getSeconds() + stepSec); 
     }
-}
 
-// ============================================= SET/GET
-// defString could be: "ld" or "hd"
-//
-function setDefinition (defString) {
-    getHttp("scene.yaml", function(err, res){
-        if (err) {
-            console.error(err);
+    var width = 98;
+    var height = 4;
+
+    // // Using Canvas
+    // var canvas = document.createElement("canvas");
+    // canvas.height = height;
+    // canvas.width = width;
+    // var ctx = canvas.getContext('2d');
+    // var imageData = ctx.getImageData(0, 0, width, height);
+    // var data = imageData.data;
+    // for (var y = 0; y < height; y++) {
+    //     for (var x = 0; x < width; x++) {
+    //         var index = (y*width+x)*4;
+    //         data[index] = 255;
+    //         data[index+1] = 0;
+    //         data[index+2] = 0;
+    //         data[index+3] = 255;
+    //     }
+    // }
+    // ctx.putImageData(imageData, 0, 0);
+    // scene.styles.orbit.shaders.uniforms.u_lut = canvas.toDataURL();
+    // scene.rebuild();
+
+    // // Using WebGL
+    var gl = scene.gl;
+    var floatTextures = gl.getExtension('OES_texture_float');
+    if (!floatTextures) {
+        console.log('no floating point texture support');
+    } else {
+        console.log('floating point texture support');
+    }
+
+    var uniforms = {};
+    uniforms.u_lut = {};
+
+    var pixels = new Float32Array(width*height*4);
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+            var index = (y*width+x)*4;
+            var data = [ .5+(track[x].ln/180)*.5, .5+(track[x].lt/90)*.5];
+            pixels[index] = data[0];
+            pixels[index+1] = data[1];
+            pixels[index+2] = 0.0;
+            pixels[index+3] = 1.0;
         }
-        var content = res.replace(/\-[x]*hd\.jpg/gm, "-"+defString+".jpg");
-        var url = createObjectURL(new Blob([content]));
-        scene.load(url,false);
-        initOrbit();
-    });
-}
-
-// ============================================= EVENTS
-function onMouseUpdate (e) {
-    var mouse = [ (e.pageX/screen.width-.5)*0.005, (e.pageY/screen.height-.5)*-0.002];
-    offset_target[0] = mouse[0];
-    offset_target[1] = mouse[1];
-}
-
-function onMotionUpdate (e) {
-    var accX = Math.round(event.accelerationIncludingGravity.x*10)/10;  
-    var accY = Math.round(event.accelerationIncludingGravity.y*10)/10;  
-    offset_target = [ -accX/1000, -accY/1000 ];
+    }
+    var texture = scene.styles.orbit.program.uniforms.u_lut;
+    gl.activeTexture(gl.TEXTURE0);
+    // gl.bindTexture(gl.TEXTURE_2D, 0);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texImage2D(
+        gl.TEXTURE_2D, // target
+        0, // mip level
+        gl.RGBA, // internal format
+        width, height, // width and height
+        0, // border
+        gl.RGBA, //format
+        gl.FLOAT, // type
+        pixels // texture data
+    );
+    // gl.bindTexture(gl.TEXTURE_2D, null);
+    scene.rebuild();
 }
